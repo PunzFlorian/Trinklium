c467a56f7423b337e533f8d6c87f01e1a691c4b0
ClementsWagner <32094981+ClementsWagner@users.noreply.github.com>
2018-02-20 23:20:29 +0100

    Add files via upload

---
+++ Project/Items/Armor/AppleHead.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinklium.Items.Armor
+{
+
+    [AutoloadEquip(EquipType.Head)]
+    class AppleHead : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Apple Helmet");
+            //Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 20;
+            item.rare = 1;
+            item.vanity = true;
+        }
+    }
+}
