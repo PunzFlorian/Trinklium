1092a066fc21ccaa9cb3f73f520e662bf2c27aa9
ClementsWagner <32094981+ClementsWagner@users.noreply.github.com>
2018-02-20 15:48:23 +0100

    Add files via upload

---
+++ Project/Projectiles/Banano.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinklium.Projectiles
+{
+    class Banano : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.YoyosLifeTimeMultiplier[projectile.type] = 3.5f;          
+            ProjectileID.Sets.YoyosMaximumRange[projectile.type] = 300f;            
+            ProjectileID.Sets.YoyosTopSpeed[projectile.type] = 13f;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.extraUpdates = 0;
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.aiStyle = 99;
+            projectile.friendly = true;
+            projectile.penetrate = -1;
+            projectile.melee = true;
+            projectile.scale = 1f;
+        }    
+    }
+}
---
+++ Project/Projectiles/Minion.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinklium.Projectiles
+{
+    public abstract class Minion : ModProjectile
+    {
+        public override void AI()
+        {
+            CheckActive();
+            Behavior();
+        }
+        public abstract void CheckActive();
+        public abstract void Behavior();
+    }
+
+}
