97b84e59d94a760810f9a2aaf92678af3e2788ab
ClementsWagner <32094981+ClementsWagner@users.noreply.github.com>
2018-02-20 19:22:45 +0100

    Add files via upload

---
+++ Project/Buffs/IronskinPlus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinklium.Buffs
+{
+    class IronskinPlus : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("IronskinPlus");
+            Description.SetDefault("Increase defense by 12");
+            Main.debuff[Type] = false;
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+            canBeCleared = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.ClearBuff(BuffID.Ironskin);
+            player.statDefense += 12;
+        }
+    }
+}
---
+++ Project/Buffs/MiningPlus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinklium.Buffs
+{
+    class MiningPlus : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("MiningPlus");
+            Description.SetDefault("50% increased Mining speed");
+            Main.debuff[Type] = false;
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+            canBeCleared = false;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.ClearBuff(BuffID.Mining);
+            player.pickSpeed = player.pickSpeed / 2;
+        }       
+    }
+}
