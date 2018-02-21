268446947fb09b3025bad049d2d03f105e8d8e94
ClementsWagner <32094981+ClementsWagner@users.noreply.github.com>
2018-02-21 15:06:16 +0100

    Add files via upload

---
+++ Project/Items/Armor/AppleBreastplate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinklium.Items.Armor
+{
+
+    [AutoloadEquip(EquipType.Body)]
+    class AppleBreastplate : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Apple Breastplate");
+            //Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 20;
+            item.rare = 1;
+            item.vanity = false;
+            item.defense = 10;
+        }
+    }
+}
---
+++ Project/Items/Armor/AppleLeggings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trinklium.Items.Armor
+{
+
+    [AutoloadEquip(EquipType.Legs)]
+    class AppleLeggings : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Apple Leggings");
+            //Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 20;
+            item.rare = 1;
+            item.vanity = false;
+            item.defense = 7;
+        }
+    }
+}
